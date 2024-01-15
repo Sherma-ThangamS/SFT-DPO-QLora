4c5e922ce835275a48654ab609f11dfd0b2545d7
Sherma Thangam S <119473142+Sherma-ThangamS@users.noreply.github.com>
2024-01-15 22:06:35 +0530

    Update __init__.py

--- sft_dpo_qlora/__init__.py
+++ sft_dpo_qlora/__init__.py
@@ -1,4 +1,4 @@
-from sft_dpo_qlora.configSFT import stfConfig
+from sft_dpo_qlora.configSFT import sftConfig
 from sft_dpo_qlora.configDPO import dpoConfig
 from sft_dpo_qlora.trainerSFT import sftTrainer
 from sft_dpo_qlora.trainerDPO import dpoTrainer
