3e2a505ee92f3ba8249b29ef2fd49da96217a754
Sherma Thangam S <119473142+Sherma-ThangamS@users.noreply.github.com>
2024-01-16 09:00:28 +0530

    Update trainerSFT.py

--- sft_dpo_qlora/trainerSFT.py
+++ sft_dpo_qlora/trainerSFT.py
@@ -3,7 +3,6 @@ from datasets import load_dataset, Dataset
 from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
 from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig, TrainingArguments
 from trl import SFTTrainer
-from configSFT import Config  # Import the Config class from config.py
 from huggingface_hub import login
 login()
 class sftTrainer:
